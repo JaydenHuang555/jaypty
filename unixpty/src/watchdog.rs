827f07dbd8f387655026a4971a935bc4833b587e
FriendlyAbby <abbyfriendly2009@proton.me>
2026-05-11 04:39:23 -0700

    watchdog errs

--- unixpty/src/watchdog.rs
+++ unixpty/src/watchdog.rs
@@ -3,11 +3,12 @@ use std::{
     io::{ErrorKind, Read},
     os::unix::net::UnixStream,
     sync::mpsc::Receiver,
+    time::Duration,
 };
 
 use jaypty_core::{
     OsEmptyResult, OsError, OsResult, SystemError,
-    child::{ChildPollRegisterIO, ChildWatchDogIO},
+    child::{ChildPollRegisterIO, ChildStatus, ChildWatchDogIO},
 };
 use libc::SIGCHLD;
 use polling::{PollMode, Poller};
@@ -19,7 +20,7 @@ use signal_hook::{
 pub struct SignalWatchDogIO {
     listener: UnixStream,
     sig_id: SigId,
-    detected_signal: bool,
+    status: ChildStatus,
 }
 
 impl Drop for SignalWatchDogIO {
@@ -29,14 +30,18 @@ impl Drop for SignalWatchDogIO {
 }
 
 impl SignalWatchDogIO {
-    pub fn spawn(signal: c_int) -> super::Result<Self> {
-        let (pipe, listener) = UnixStream::pair()?;
-        let sig_id = pipe::register(signal, pipe)?;
-        listener.set_nonblocking(true)?;
+    pub fn spawn(signal: c_int) -> OsResult<Self> {
+        let (pipe, listener) =
+            UnixStream::pair().map_err(SystemError::SpawnChildExitListenerStreamFailure)?;
+        let sig_id =
+            pipe::register(signal, pipe).map_err(SystemError::SpawnChildPipeCallBackFailure)?;
+        listener
+            .set_nonblocking(true)
+            .map_err(SystemError::SpawnChildExitListenerStreamFailure)?;
         Ok(Self {
             listener,
             sig_id,
-            detected_signal: false,
+            status: ChildStatus::Alive,
         })
     }
 }
@@ -74,43 +79,41 @@ impl ChildPollRegisterIO<OsError> for SignalWatchDogIO {
 impl ChildWatchDogIO<SystemError> for SignalWatchDogIO {
     /// TODO: add support for getting the exit code from the child
     /// if it does exit
-    fn status(&mut self) -> Option<OsResult> {
-        if self.detected_signal {
-            Some(Ok(0))
+    fn status(&mut self) -> OsResult<ChildStatus> {
+        if self.status.is_dead() {
+            return Ok(self.status);
         } else {
             let mut buff = [0u8; 1];
             match self.listener.read(&mut buff) {
                 Ok(n) => {
                     if n > 0 {
-                        self.detected_signal = true;
-                        return Some(Ok(0));
+                        self.status = ChildStatus::Dead(0);
                     }
                 }
-                Err(e) => {
-                    return None;
+                Err(_) => {}
+            }
+            Ok(self.status)
+        }
+    }
+
+    fn is_dead(&self) -> OsResult<ChildStatus> {
+        Ok(self.status)
+    }
+
+    fn wait(&mut self) -> OsResult<ChildStatus> {
+        if self.status.is_dead() {
+            return Ok(self.status);
+        }
+
+        let last_status = self.status;
+        loop {
+            std::thread::sleep(Duration::from_millis(200));
+            if self.status()? != last_status {
+                if self.status.is_dead() {
+                    break;
                 }
             }
-
-            None
         }
-    }
-
-    fn is_dead(&self) -> bool {
-        self.detected_signal
-    }
-
-    fn wait(&mut self) {
-        self.listener.set_nonblocking(false).unwrap();
-        let mut buff = [0u8; 1];
-        match self.listener.read(&mut buff) {
-            Ok(e) => {
-                self.listener.set_nonblocking(true).ok();
-                return;
-            }
-            Err(_) => {
-                self.listener.set_nonblocking(true).ok();
-                return;
-            }
-        }
+        return Ok(self.status);
     }
 }
