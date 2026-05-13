1a342412ab0e705be901f3342d1f4defa5c9148b
Jayden <179274165+JaydenHuang555@users.noreply.github.com>
2026-05-13 11:23:17 -0700

    child killer

--- ptywin/src/child/killer.rs
+++ ptywin/src/child/killer.rs
@@ -1,9 +1,20 @@
-use std::{ffi::c_void, sync::atomic::AtomicPtr, thread};
+use std::{
+    ffi::c_void,
+    io,
+    sync::atomic::{AtomicPtr, Ordering},
+    thread,
+};
 
-use jaypty_core::child::{consume::ConsumedChildConsumer, killer::ConsumedChildKiller};
-use windows_sys::Win32::System::Threading::TerminateProcess;
+use jaypty_core::{
+    ErrorFactory, FactoriedError, FactoriedErrorKind, SystemError,
+    child::{ChildStatus, consume::ConsumedChildConsumer, killer::ConsumedChildKiller},
+};
+use windows_sys::Win32::{
+    Foundation::WAIT_FAILED,
+    System::Threading::{GetExitCodeProcess, INFINITE, TerminateProcess, WaitForSingleObject},
+};
 
-use crate::child::ChildHandle;
+use crate::child::{ChildHandle, WinChildWatchdogIO};
 
 pub struct ConsumedContpyChildKiller(ChildHandle);
 
@@ -17,7 +28,23 @@ impl ConsumedChildKiller for ConsumedContpyChildKiller {
     fn blocking(self) -> jaypty_core::Result<jaypty_core::child::ChildStatus> {
         let handle = self.consume();
         let _ = unsafe { TerminateProcess(handle.load(std::sync::atomic::Ordering::Relaxed), 1) };
-        todo!()
+        let wait_stat = unsafe { WaitForSingleObject(handle.load(Ordering::Relaxed), INFINITE) };
+        if wait_stat == WAIT_FAILED {
+            return Err(ErrorFactory::kind(FactoriedErrorKind::ChildKillFailed)
+                .with_internal(SystemError::ChildFailedRegisterWait(wait_stat)));
+        }
+        let mut exit_code = 0u32;
+        let stat = unsafe {
+            GetExitCodeProcess(handle.load(Ordering::Relaxed), &mut exit_code as *mut u32)
+        };
+        if stat == 0 {
+            return Err(
+                ErrorFactory::kind(FactoriedErrorKind::ChildKillFailed).with_internal(
+                    SystemError::ChildFailedToGetExitCode(io::Error::last_os_error()),
+                ),
+            );
+        }
+        Ok(ChildStatus::Dead(exit_code as i32))
     }
 
     fn nonblocking(
